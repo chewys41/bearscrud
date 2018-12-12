bad8df26bdf97f2a7d73e5087da9356d58da149b
Robert Crum <gargansa@hotmail.com>
2018-12-11 19:55:46 -0700

    Nice Eyes

--- app/Controllers/Http/BearController.js
+++ app/Controllers/Http/BearController.js
@@ -1,4 +1,5 @@
-'use strict'
+'use strict'  
+// when typing use strict you have to have const and let
 let bears = [{ id: '0', Type: 'Grizzly', Name: 'Al', Weight: '1000lb' }]
 
 class BearController {
@@ -11,6 +12,7 @@ class BearController {
         const bear = request.post()
         
         bears.push(bear)
+        response.send(bears)
     }
     
     beardelete({request, response}) {
@@ -19,6 +21,24 @@ class BearController {
         bears = bears.filter((eachItem) => {
             return eachItem.id !== body.id
         })
+        response.send(bears)
+    }
+    bearupdate({request,response}){
+
+        // Grab info from postman and save it into body
+        const body = request.post()
+
+        //Map or iterate over each bear
+        const newBears = bears.map((thisBear)=>{
+            //check to see if the current bear we are mapping over is the same id as the info from postman
+            if(thisBear.id === body.id){
+                return body
+            }else{
+                return thisBear
+            }
+        })
+        response.send(newBears)
+
     }
 }
 
